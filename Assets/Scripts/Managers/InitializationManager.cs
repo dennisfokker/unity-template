7c1fbf7a23e6e85ebe9b678ca98fd4de23e584d8
TheDutcher <dennis.fokker@live.nl>
2017-12-05 16:58:38 +0100

    Forced text asset serialization. Created manager initialization script.

---
+++ Assets/Scripts/Managers/InitializationManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitializationManager : MonoBehaviour
+{
+    private const string InitializationSceneName = "InitGame";
+    private static string SceneToReload = "Splash";
+
+    void Start()
+    {
+        LeanTween.init();
+
+        DelayUtil.WaitForFrame(InitializeGame);
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void ForceLoadInitializeGameScene()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        if (currentSceneName != InitializationSceneName)
+            SceneToReload = currentSceneName;
+
+        SceneManager.LoadScene(InitializationSceneName);
+    }
+
+    private static void InitializeGame()
+    {
+        SceneManager.LoadScene(SceneToReload);
+    }
+}
