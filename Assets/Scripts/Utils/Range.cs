a96e6f8ed402ab829c2d37c6bcf47d0a362f458d
TheDutcher <dennis.fokker@live.nl>
2019-01-23 02:04:41 +0100

    Added implicit casting between ranges.

--- Assets/Scripts/Utils/Range.cs
+++ Assets/Scripts/Utils/Range.cs
@@ -124,42 +124,102 @@ public abstract class Range<T> where T : IComparable<T>
         hashCode = hashCode * -1521134295 + EqualityComparer<T>.Default.GetHashCode(maximum);
         return hashCode;
     }
-
-    #region Non-generic implementations to allow serialization
-    [Serializable]
-    public class DoubleRange : Range<double>
-    {
-        public DoubleRange(double minimum, double maximum) : base(minimum, maximum)
-        {
-            // Nothing special to do
-        }
-    }
-
-    [Serializable]
-    public class FloatRange : Range<float>
-    {
-        public FloatRange(float minimum, float maximum) : base(minimum, maximum)
-        {
-            // Nothing special to do
-        }
-    }
-
-    [Serializable]
-    public class IntRange : Range<int>
-    {
-        public IntRange(int minimum, int maximum) : base(minimum, maximum)
-        {
-            // Nothing special to do
-        }
-    }
-
-    [Serializable]
-    public class LongRange : Range<long>
-    {
-        public LongRange(long minimum, long maximum) : base(minimum, maximum)
-        {
-            // Nothing special to do
-        }
-    }
-    #endregion
 }
+
+#region Non-generic implementations to allow serialization
+[Serializable]
+public class DoubleRange : Range<double>
+{
+    public DoubleRange(double minimum, double maximum) : base(minimum, maximum)
+    {
+        // Nothing special to do
+    }
+
+    public static implicit operator DoubleRange(FloatRange range)
+    {
+        return new DoubleRange(range.Minimum, range.Maximum);
+    }
+
+    public static implicit operator DoubleRange(IntRange range)
+    {
+        return new DoubleRange(range.Minimum, range.Maximum);
+    }
+
+    public static implicit operator DoubleRange(LongRange range)
+    {
+        return new DoubleRange(range.Minimum, range.Maximum);
+    }
+}
+
+[Serializable]
+public class FloatRange : Range<float>
+{
+    public FloatRange(float minimum, float maximum) : base(minimum, maximum)
+    {
+        // Nothing special to do
+    }
+
+    public static implicit operator FloatRange(DoubleRange range)
+    {
+        return new FloatRange((float) range.Minimum, (float) range.Maximum);
+    }
+
+    public static implicit operator FloatRange(IntRange range)
+    {
+        return new FloatRange(range.Minimum, range.Maximum);
+    }
+
+    public static implicit operator FloatRange(LongRange range)
+    {
+        return new FloatRange(range.Minimum, range.Maximum);
+    }
+}
+
+[Serializable]
+public class IntRange : Range<int>
+{
+    public IntRange(int minimum, int maximum) : base(minimum, maximum)
+    {
+        // Nothing special to do
+    }
+
+    public static implicit operator IntRange(DoubleRange range)
+    {
+        return new IntRange((int) range.Minimum, (int) range.Maximum);
+    }
+
+    public static implicit operator IntRange(FloatRange range)
+    {
+        return new IntRange((int) range.Minimum, (int) range.Maximum);
+    }
+
+    public static implicit operator IntRange(LongRange range)
+    {
+        return new IntRange((int) range.Minimum, (int) range.Maximum);
+    }
+}
+
+[Serializable]
+public class LongRange : Range<long>
+{
+    public LongRange(long minimum, long maximum) : base(minimum, maximum)
+    {
+        // Nothing special to do
+    }
+
+    public static implicit operator LongRange(DoubleRange range)
+    {
+        return new LongRange((long) range.Minimum, (long) range.Maximum);
+    }
+
+    public static implicit operator LongRange(FloatRange range)
+    {
+        return new LongRange((long) range.Minimum, (long) range.Maximum);
+    }
+
+    public static implicit operator LongRange(IntRange range)
+    {
+        return new LongRange(range.Minimum, range.Maximum);
+    }
+}
+#endregion
